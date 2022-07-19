c391ef6dc6e7b338c25e44b14fd82b25a428b842
Danny Robinson <dannyrobinson@gmx.com>
2022-07-19 12:26:25 +0100

    Initial commit

---
+++ empty_server.js
@@ -0,0 +1,32 @@
+
+/**
+ * This function should return a string that will be used as the server's name. 
+ * It must be short enough to fit in the NETronics Connect! menu.
+ **/
+function getName() {
+  return 'Server Name'
+}
+
+/** 
+ * This function will be called when a user connects to the server. 
+ **/
+function onConnect() {
+
+}
+
+/**
+ * This function will be called approximately 30 times a
+ * second while a user is connected to the server.
+ **/
+function onUpdate() {
+
+}
+
+
+/**
+ * This function will be called every time the connected user presses a key. 
+ * @param {number} key the ASCII representation of the key pressed
+ **/
+function onInput(key) {
+
+}
---
+++ index.d.ts
@@ -0,0 +1,83 @@
+declare const enum Color {
+  BLACK,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  WHITE,
+}
+
+/** Clear the screen. */
+declare function clearScreen(): void
+
+/**
+ * Draw the specified text. The color parameter is a
+ * number between 0 (darkest) and 17 (lightest).
+ **/
+declare function drawText(
+  text: string,
+  color: Color,
+  x: number,
+  y: number
+): void
+
+/**
+ * Draw the specified text, wrapping it so that it is no
+ * more than width characters wide.
+ **/
+declare function drawTextWrapped(
+  text: string,
+  color: Color,
+  x: number,
+  y: number,
+  width: number
+): void
+
+/**
+ * Draw a box using the built-in box drawing characters.
+ **/
+declare function drawBox(
+  color: Color,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+): void
+
+/**
+ * Fill an area using the specified symbol.
+ **/
+declare function fillArea(
+  symbol: string,
+  color: Color,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+): void
+
+/**
+ * Write this server's persisted data string. You can
+ * convert a JavaScript object to a JSON string using
+ * `JSON.stringify()`.
+ **/
+declare function saveData(data: string): void
+
+/**
+ * Read this server's persisted data string. You can
+ * convert a JSON string to a JavaScript object using
+ * `JSON.parse()`.
+ **/
+declare function loadData(): string
